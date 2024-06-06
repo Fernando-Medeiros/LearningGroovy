c4f9227f911cfe4223339a9dc857b1c95d2677dc
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-06-06 00:21:28 -0300

    feat: + It;

--- src/Closures/ItExamples.groovy
+++ src/Closures/ItExamples.groovy
@@ -1,18 +1,27 @@
 package Closures
 
 class ItExamples {
-    var name = "It Class";
+    var name = "Iteration Examples"
     var numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
 
     ItExamples() {
-        Show.call numbers.collect {
-            it % 2 == 0 || it
+        Show.call numbers.collect { number ->
+            number % 2 == 0 || number
         }
-        Show.call numbers.each {
-            Show.call it
+        Show.call numbers.each { number ->
+            Show.call number
         }
-        Show.call numbers.findAll {
-            it % 2 == 0
+        Show.call numbers.eachWithIndex{ int entry, int idx ->
+            Show.call "$idx: $entry"
+        }
+        Show.call numbers.findAll { number ->
+            number % 2 == 0
+        }
+        Show.call numbers.find { number ->
+            number == 3
+        }
+        Show.call numbers.sort { number ->
+            number < 3
         }
         Show.call numbers.remove(9)
         Show.call numbers.contains(12)
@@ -20,6 +29,7 @@ class ItExamples {
         Show.call name
         Show.call Sum.call(50, 100)
     }
+
     def Show = {
         println "$it"
     }
